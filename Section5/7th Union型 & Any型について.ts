11c4f51ca34e22fbdb3f0f79d2c2a25c5cd6e107
xyytgae <xyytgae@yahoo.co.jp>
2020-11-14 10:35:16 +0900

    #2 セクション5:高等な型

---
+++ Section5/7th Union型 & Any型について.ts
@@ -0,0 +1,11 @@
+let a: number | string | boolean
+
+a = 1
+a = 'hello'
+a = true
+
+let b: any
+
+b = 1
+b = '2'
+b = true
---
+++ Section5/9th Literal型について.ts
@@ -0,0 +1,5 @@
+let a: 'hello' = 'hello'
+// a = 'こんにちは' //←NG
+
+let number: 10 = 10
+let boolean: true = true
