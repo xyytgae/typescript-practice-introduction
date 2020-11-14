aa66326cf2ead2978d37cef6b29809119a9d5c91
xyytgae <xyytgae@yahoo.co.jp>
2020-11-14 10:44:53 +0900

    #3 セクション6:型のエイリアス

---
+++ Section6/10th 型のエイリアスについて.ts
@@ -0,0 +1,13 @@
+
+type Name = string
+let firstName: Name = '太郎'
+let lastName: Name = '山田'
+
+type Password = string | number
+let password: Password = 'ljkkhrkyh7647'
+
+type User = {id: number, name: string}
+const user: User ={
+    id: 0,
+    name: 'yamada'
+}
